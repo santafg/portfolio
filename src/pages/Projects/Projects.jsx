425ae2b19e8f63a2db4eb7df7da3fedc661bdb52
santafg <santanughosh28111998@gmail.com>
2021-12-03 22:03:26 +0530

    resume link change

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import works from "../../images/works.png";
-
-
-import project1 from "../../images/nc.JPG";
-import project2 from '../../images/fbsocial.JPG'
-import project3 from '../../images/scom.JPG'
 
 import "./Projects.css";
+import { projects } from "../../projectList";
 
 const Projects = () => {
   return (
@@ -18,49 +13,28 @@ const Projects = () => {
               <h1>Projects</h1>
             </div>
             <div className="projects">
-            <div className="project">
-                <div className="imageDiv">
-                  <img src={project3} alt="Project1" className="project1" />
+              {projects.map((project, i) => (
+                <div className="row" key={i}>
+                  <div className="project">
+                    <div className="imageDiv">
+                      <img
+                        src={project.image}
+                        alt={`${project.name}`}
+                        className="project1"
+                      />
+                    </div>
+                    <div className="description">
+                      <h2>{project.name}</h2>
+                      <p>{project.description}</p>
+                    </div>
+                    <div className="btn">
+                      <a href={project.link} target="blank">
+                        <button>Visit</button>
+                      </a>
+                    </div>
+                  </div>
                 </div>
-                <div className="description">
-                  <h2>Secommerce</h2>
-                  <p>It is a responsive dynamic ecommerce site like Amazon . </p>
-                </div>
-                <div className="btn">
-                <a href="https://secom.netlify.app/" target="_blank">
-                  <button>Visit</button>
-                </a>
-                </div>
-              </div>
-              <div className="project">
-                <div className="imageDiv">
-                  <img src={project1} alt="Project1" className="project1" />
-                </div>
-                <div className="description">
-                  <h2>Note's Cloud</h2>
-                  <p>A Responsive Webapp to save personal notes online , a particular user must have to register first for saving the notes. </p>
-                </div>
-                <div className="btn">
-                <a href="https://notes-cloud.herokuapp.com" target="_blank">
-                  <button>Visit</button>
-                </a>
-                </div>
-              </div>
-              <div className="project">
-                <div className="imageDiv">
-                  <img src={project2} alt="Project1" className="project1" />
-                </div>
-                <div className="description">
-                  <h2>Social media UI</h2>
-                  <p>A simple React Facebook UI clone with Timeline and Profile page. </p>
-                </div>
-                <div className="btn">
-                <a href="https://fbsimpleui.netlify.app" target="_blank">
-                  <button>Visit</button>
-                </a>
-                </div>
-              </div>
-              
+              ))}
             </div>
           </div>
         </div>
