d02eaf8190b40dc4ba53a2ee7d67881df00cc754
Santanu Ghosh <78014920+santafg@users.noreply.github.com>
2022-12-08 17:35:35 +0530

    updated about 8-12-22

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -16,17 +16,11 @@ const About = () => {
               </div>
               <div className="details">
                 <p>
-                  I'm from Saltlake city (Kolkata) , completed my 
-                  B.Tech in the field Electrical Engineering , beside that I have a
-                  great potential towards Web Devlopment .
+                  I'm from Saltlake city (Kolkata) , completed my B.Tech in the field of Electrical Engineering , beside that I have a great potential and passion towards Software Devlopment .
                 </p>
-                <p> I started learning
-                  Web Devlopment from Year 2020 and fell in love with the
-                  concepts of Web Devlopment and started exploring the
-                  technologies like 'Reactjs' , 'Nodejs' , 'Expressjs' ,
-                  'MongoDB' , after gathering some basic knowledge about those
-                  technologies I started to build my own projects .</p>
-              </div>
+                <p>  I started learning Web Devlopment from Year 2020 and fell in love with the concepts of Web Devlopment and then started exploring the technologies like 'Reactjs' , 'Nodejs' , 'Expressjs' , 'MongoDB' , after gathering some basic knowledge about those technologies and I started to build my own projects .</p>
+    <p> From 2022 I focused my self towards mobile app development with help of technologies like React Native , Expo and Ionic .</p>         
+    </div>
               <div className="btn">
                 <Link
                   to="projects"
